d5d072f800006f3e16d94564148a4145e78ebecc
lb42linux <136691069+lb42linux@users.noreply.github.com>
2023-08-08 21:53:15 +0300

    Update kullanici_sil.sh

--- kullanici_sil.sh
+++ kullanici_sil.sh
@@ -13,9 +13,12 @@ if echo "$currp" | sudo -S echo "" >/dev/null 2>&1; then
     if [ -n "$DATA" ]; then
         sudo userdel -r "$DATA"
         zenity --info --title="Kullanıcı Sil" --text="İşlem Başarılı"
+        bash ./LB42-ayarlar-programcigi.sh 
     else
         zenity --error --title="Hata" --text="Bir sorun oluştu böyle bir hesap olmayabilir."
+        bash ./LB42-ayarlar-programcigi.sh 
     fi
 else
-    zenity --error --title="Hata" --text="İşlem Başarısız."
+    zenity --error --title="Hata" --text="Şifre Yalnış veya işlem başarısız."
+    bash ./LB42-ayarlar-programcigi.sh 
 fi
