34605fad1fd4c080c780531f744fd0193e4e7b8a
lb42linux <136691069+lb42linux@users.noreply.github.com>
2023-08-08 21:52:07 +0300

    Update LB42-ayarlar-programcigi.sh

--- LB42-ayarlar-programcigi.sh
+++ LB42-ayarlar-programcigi.sh
@@ -3,7 +3,7 @@
 zenity --list --title="Resim Seçin" --column="Resim" --icon="/path/to/image.png" "Resim 1" "Resim 2" "Resim 3"
 
 
-selection=$(zenity --list --title="Program Seçimi" --text="Aşağıdan bir program seçin:" --column="LB42 Ayarlar Programcığı" "Kullanıcı Oluştur" "Kullanıcı Sil" "Parola Değiştir" "Sistemi Güncelle")
+selection=$(zenity --list --title="Program Seçimi" --text="Aşağıdan bir program seçin:" --column="LB42 Ayarlar Programcığı" "Kullanıcı Oluştur" "Kullanıcı Sil" "Parola Değiştir" "Sistemi Güncelle" "Pencere Opaklığını Ayarla" "XFCE4 Panel Ayarları" "Diğer Ayarlar (XFCE4 Settings)")
 
 case "$selection" in
     "Kullanıcı Oluştur")
@@ -14,6 +14,12 @@ case "$selection" in
         bash parola_degistir.sh ;;
     "Sistemi Güncelle")
         bash sistem_guncelleme.sh ;;
+    "Pencere Opaklığını Ayarla")
+        bash opaklik_ayarla.sh ;;
+    "XFCE4 Panel Ayarları")    
+        xfce4-panel --preferences ;;
+    "Diğer Ayarlar (XFCE4 Settings)") 
+        xfce4-settings-manager ;;
     *)
         
 esac
